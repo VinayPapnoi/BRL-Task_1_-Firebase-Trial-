285ed19639399b08c484605f806651b8dca46840
VinayPapnoi <papnoi.vinay@gmail.com>
2025-09-17 14:41:25 +0530

    trials

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:currency_converter/currency_converter_material_page.dart';
+//import 'package:currency_converter/currency_converter_material_page.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:currency_converter/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -10,8 +18,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CurrencyConverterMaterialPage(),
-    );
+    return const MaterialApp(home: SignupScreen());
   }
 }
